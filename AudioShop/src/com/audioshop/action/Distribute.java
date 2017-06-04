b54ba594cb4d7ae786f6d27bb4ab8183ee1d80f4
imu-yangyuan <yangyuan@imudges.com>
2017-06-04 18:21:13 +0800

    Revert "addvip分发"
    This reverts commit 056e86962bde4984f71317a038510b033b06fb80.

--- AudioShop/src/com/audioshop/action/Distribute.java
+++ AudioShop/src/com/audioshop/action/Distribute.java
@@ -3,9 +3,5 @@ package com.audioshop.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class Distribute extends ActionSupport{
- public String addVip(){
-	 
-	 //return "addvip";
-	 return "login";
- }
+
 }
