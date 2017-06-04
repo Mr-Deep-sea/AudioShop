ce617361cfb41ba81c648ca65859b0b00f2621f6
duola-shuang <1342741446@qq.com>
2017-06-04 13:40:12 +0800

    Service.impl的第二部分

--- AudioShop/src/com/audioshop/service/impl/OrderServiceImpl.java
+++ AudioShop/src/com/audioshop/service/impl/OrderServiceImpl.java
@@ -1,8 +1,22 @@
 package com.audioshop.service.impl;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.audioshop.dao.AudioDao;
+import com.audioshop.dao.OrderDao;
 import com.audioshop.model.Order;
 import com.audioshop.service.OrderService;
 
+@Service("orderService")
 public class OrderServiceImpl extends BaseServiceImpl<Order> implements OrderService{
 
+	private OrderDao orderDao;
+	
+	@Resource
+	public void setOrderDao(OrderDao orderDao) {
+		super.setBaseDao(orderDao);
+		this.orderDao = orderDao;
+	}
 }
--- AudioShop/src/com/audioshop/service/impl/ReservationRecordServiceImpl.java
+++ AudioShop/src/com/audioshop/service/impl/ReservationRecordServiceImpl.java
@@ -1,8 +1,35 @@
 package com.audioshop.service.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.audioshop.dao.AudioDao;
+import com.audioshop.dao.ReservationRecordDao;
+import com.audioshop.dao.UserDao;
 import com.audioshop.model.ReservationRecord;
+import com.audioshop.model.User;
 import com.audioshop.service.ReservationRecordService;
 
+@Service("reservationService")
 public class ReservationRecordServiceImpl  extends BaseServiceImpl<ReservationRecord> implements ReservationRecordService{
+
+	private ReservationRecordDao reservationRecordDao;
+	private AudioDao audioDao;
+
+	@Resource
+	public void setAudioDao(AudioDao audioDao) {
+		this.audioDao = audioDao;
+	}
+
+	@Resource
+	public void setReservationRecordDao(ReservationRecordDao reservationRecordDao) {
+		super.setBaseDao(reservationRecordDao);
+		this.reservationRecordDao = reservationRecordDao;
+	}
+	
+
 
 }
