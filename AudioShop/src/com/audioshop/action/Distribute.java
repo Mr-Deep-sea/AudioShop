056e86962bde4984f71317a038510b033b06fb80
imu-zhangzhiqiang <601843423@qq.com>
2017-06-04 18:03:47 +0800

    addvip分发

--- AudioShop/src/com/audioshop/action/Distribute.java
+++ AudioShop/src/com/audioshop/action/Distribute.java
@@ -3,5 +3,9 @@ package com.audioshop.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class Distribute extends ActionSupport{
-
+ public String addVip(){
+	 
+	 //return "addvip";
+	 return "login";
+ }
 }
